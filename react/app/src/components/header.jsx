8172396cc127bf5a07158be382ac4c7563177818
marnysan111 <o-masato@outlook.jp>
2021-09-21 06:06:32 +0000

    headerの追加(途中)

---
+++ react/app/src/components/header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../static/css/header.css';
+
+export default function Header(){
+    return(
+        <div className="header">
+            <p>This is Header</p>
+        </div>
+    )
+}
