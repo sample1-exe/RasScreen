3740113858087051d3bcb377d7b9ea249aa0e2fc
marnysan111 <o-masato@outlook.jp>
2021-09-25 14:05:25 +0000

    material-ui 壊れてる

--- react/app/src/App.js
+++ react/app/src/App.js
@@ -7,11 +7,7 @@ import {Grid} from '@material-ui/core/';
 function App() {
   return (
     <React.Fragment>
-      <Grid container>
-        <Grid item xs={12}>
           <Header  />
-        </Grid>
-      </Grid>
     </React.Fragment>
   );
 }
--- react/app/src/components/header.jsx
+++ react/app/src/components/header.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import '../static/css/header.css';
+import Icon from '../static/img/RasScreenIcon/logo_R.jpg';
+import Image from'react-image-resizer';
+import {Grid, Box} from '@material-ui/core/';
+import { makeStyles } from '@material-ui/styles';
+
 
 export default function Header(){
+    const Style = {
+        Image: {
+            
+        }
+    }
     return(
-        <div className="header">
-            <p>This is Header</p>
-        </div>
+        <Grid container className="header"　alignItems="center" justify="center">
+            <Grid item xs={1}>
+                <Box>
+                    <Image src={Icon} alt="Logo"
+                    height={ 40 }
+                    width={ 40 }
+                    style = {Style.Image}
+                    />
+                </Box>
+            </Grid>
+            <Grid item xs={9}>
+            めぬー
+            </Grid>
+            <Grid item xs={1}>
+            <AccessTimeIcon />
+            </Grid>
+        </Grid>
     )
 }
