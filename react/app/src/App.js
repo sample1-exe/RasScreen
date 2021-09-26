32cbcc28104e5f25904ea88a4fd7d4e37e015f46
marnysan111 <o-masato@outlook.jp>
2021-09-26 09:23:11 +0000

    中央揃え

--- react/app/src/App.js
+++ react/app/src/App.js
@@ -7,7 +7,7 @@ import {Grid} from '@material-ui/core/';
 function App() {
   return (
     <React.Fragment>
-          <Header  />
+          <Header />
     </React.Fragment>
   );
 }
--- react/app/src/components/header.jsx
+++ react/app/src/components/header.jsx
@@ -3,32 +3,30 @@ import '../static/css/header.css';
 import Icon from '../static/img/RasScreenIcon/logo_R.jpg';
 import Image from'react-image-resizer';
 import {Grid, Box} from '@material-ui/core/';
-import { makeStyles } from '@material-ui/styles';
-
+import MenuIcon from '@material-ui/icons/Menu';
 
 export default function Header(){
-    const Style = {
-        Image: {
-            
-        }
-    }
+    const style = {
+        image: {
+            margin: "0 auto"
+        },
+      };
     return(
-        <Grid container className="header"　alignItems="center" justify="center">
+        <div className="header">
+            <Grid container alignItems="center" justify="center">
             <Grid item xs={1}>
-                <Box>
-                    <Image src={Icon} alt="Logo"
-                    height={ 40 }
-                    width={ 40 }
-                    style = {Style.Image}
-                    />
-                </Box>
+                <Image src={Icon} alt="Logo"
+                height={ 40 }
+                width={ 40 }
+                style={style.image}
+                />
             </Grid>
-            <Grid item xs={9}>
-            めぬー
+            <Grid item xs={10}>
             </Grid>
             <Grid item xs={1}>
-            <AccessTimeIcon />
+                <MenuIcon />
             </Grid>
         </Grid>
+        </div>
     )
 }
