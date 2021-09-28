ba5c8480022e29ecf7fe26e83812c1d2388cfdcc
marnysan111 <o-masato@outlook.jp>
2021-09-28 06:20:11 +0000

    枠追加ver1

--- react/app/src/App.js
+++ react/app/src/App.js
@@ -1,5 +1,6 @@
 import './static/css/App.css';
 import Header from './components/header'; 
+import HostList from './components/hostlist';
 import React, { useState, useEffect } from 'react'
 import {Grid} from '@material-ui/core/';
 import RasTab from './components/tab';
@@ -8,6 +9,15 @@ function App() {
   return (
     <React.Fragment>
           <Header />
+          <Grid container alignItems="center" justify="center"> 
+            <Grid item xs={4}>
+              <HostList />
+
+            </Grid>
+            <Grid item xs={8}>
+              apjd
+            </Grid>
+          </Grid>
     </React.Fragment>
   );
 }
---
+++ react/app/src/components/hostlist.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../static/css/figma.css';
+
+export default function HostList() {
+    return (
+        <div className="HostList_frame">
+            unko
+        </div>
+    )
+}
